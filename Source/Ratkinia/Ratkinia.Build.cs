e7a607e2f3c7cf60dc1715e7eea4ad7819abce25
floweryclover <floweryclover@gmail.com>
2025-04-20 16:42:22 +0900

    OpenSSL 적용

--- Source/Ratkinia/Ratkinia.Build.cs
+++ Source/Ratkinia/Ratkinia.Build.cs
@@ -16,16 +16,25 @@ public class Ratkinia : ModuleRules
 
 		string ProjectRoot = Path.GetFullPath(Path.Combine(ModuleDirectory, "..", ".."));
 		string ThirdPartyPath = Path.Combine(ProjectRoot, "ThirdParty");
+		
 		string ProtobufPath = Path.Combine(ThirdPartyPath, "Protobuf");
 		string ProtobufIncludePath = Path.Combine(ProtobufPath, "include");
-		string ProtobufLibPath = Path.Combine(ProtobufPath, "lib");
+		string ProtobufLibPath = Path.Combine(ProtobufPath, "lib");		
+		
+		string OpenSSLPath = Path.Combine(ThirdPartyPath, "OpenSSL");
+		string OpenSSLIncludePath = Path.Combine(OpenSSLPath, "include");
+		string OpenSSLLibPath = Path.Combine(OpenSSLPath, "lib");
 
 		PublicIncludePaths.Add(ProtobufIncludePath);
+		PublicIncludePaths.Add(OpenSSLIncludePath);
 
 		foreach (var libFile in Directory.GetFiles(ProtobufLibPath, "*.lib"))
 		{
 			PublicAdditionalLibraries.Add(libFile);
-			
+		}
+		foreach (var libFile in Directory.GetFiles(OpenSSLLibPath, "*.lib"))
+		{
+			PublicAdditionalLibraries.Add(libFile);
 		}
 
 		PublicDefinitions.Add("PROTOBUF_ENABLE_DEBUG_LOGGING_MAY_LEAK_PII=0");
